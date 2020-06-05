7e6521e6868b3b034fdcf77ce695373f894f49d7
therandomfool <mddenver@protonmail.com>
2020-06-05 14:35:52 -0700

    added DB

---
+++ DB/reservations.sql
@@ -0,0 +1,48 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema eventdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `eventdb` ;
+
+-- -----------------------------------------------------
+-- Schema eventdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `eventdb` DEFAULT CHARACTER SET utf8 ;
+USE `eventdb` ;
+
+-- -----------------------------------------------------
+-- Table `reservations`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `reservations` ;
+
+CREATE TABLE IF NOT EXISTS `reservations` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(500) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS eventuser@localhost;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'eventuser'@'localhost' IDENTIFIED BY 'eventuser';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'eventuser'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `reservations`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eventdb`;
+INSERT INTO `reservations` (`id`, `name`) VALUES (1, 'Test');
+
+COMMIT;
+

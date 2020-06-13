24df28c742e1aba9b140a331ec238642287aba73
therandomfool <mddenver@protonmail.com>
2020-06-13 11:49:29 -0700

    Create reservation and display reservation by ID working

--- ReservationTracker/src/main/webapp/js/script.js
+++ ReservationTracker/src/main/webapp/js/script.js
@@ -7,13 +7,15 @@ function init(){
 	// Set up view all reservation form
 	document.resForm.lookup.addEventListener('click', function(event) {
 		event.preventDefault();
-		var resId =document.resForm.resId.value;
-		if(!isNaN(resId) && resId> 0) {
-			getAllRes(id);
+		var id =document.resForm.id.value;
+		if(!isNaN(id) && id> 0) {
+			// getAllRes(id);
+			getResDisplay(id);
 		}
 	});
+
 	// Set up reservation creation form 
-	document.resFormCreate.addRes.addEventListener('click', function(event) {
+	document.resFormCreate.create.addEventListener('click', function(event) {
 		event.preventDefault();
 		createRes();
 	})
@@ -25,7 +27,31 @@ function resError(){
 	resDiv.textContent = '';
 	let displayError = document.createElement('h1');
 	displayError.textContent = 'Reservation not found';
-	resError.appendChild(displayError);
+	resDiv.appendChild(displayError);
+}
+
+function getResDisplay(id){
+	let xhr = new XMLHttpRequest();
+	console.log(id);
+	uri = `api/reservation/${id}`;
+	xhr.open('GET', uri, true);
+	// xhr.setRequestHeader("Content-type", "application/json"); // Specify JSON request body
+	xhr.onreadystatechange = function() {
+  		if (xhr.readyState === 4 ) {
+			if ( xhr.status == 200) { // Ok or Created
+			let reserveJson = xhr.responseText;
+      		var data = JSON.parse(xhr.responseText);
+			  console.log(data);
+			  displayRes(data);
+    }
+    	else if (xhr.status === 404){
+      		console.log("GET request failed."); 
+    	} else {
+			resError();
+		}
+  }
+};
+	xhr.send(null);
 }
 
 function displayRes(reservation) {
@@ -39,42 +65,45 @@ function displayRes(reservation) {
 
 	// res time
 	let resTime = document.createElement('h2');
-	resTime.textContent = reservation.time;
+	resTime.textContent = reservation.reservationTime;
 	resDiv.appendChild(resTime);
 
 	// res phone
 	let resPhone = document.createElement('h3');
 	resPhone.textContent = reservation.phone;
-	resPhone.appendChild(resTime);
+	resDiv.appendChild(resPhone);
 
 	// res how many
 	let resHowMany = document.createElement('h3');
 	resHowMany.textContent = reservation.howMany;
-	resHowMany.appendChild(resHowMany);
+	resDiv.appendChild(resHowMany);
 
 	// res requests
 	let resRequest = document.createElement('h3');
-	resRequest.textContent = reservation.request;
-	resRequest.appendChild(resRequest);
+	resRequest.textContent = reservation.requests;
+	resDiv.appendChild(resRequest);
 
 	//  res email
 	let resEmail = document.createElement('h3');
 	resEmail.textContent = reservation.email;
-	resEmail.appendChild(resEmail);
+	resDiv.appendChild(resEmail);
 }
 
 function createRes() {
 	let form = document.resFormCreate;
 	let reserve = {};
 	reserve.name = form.name.value;
-	reserve.time = form.time.value;
+	reserve.reservationTime = form.reservationTime.value;
 	reserve.phone = form.phone.value;
 	reserve.howMany = form.howMany.value;
-	reserve.request = form.request.value;
+	reserve.requests = form.requests.value;
 	reserve.email = form.email.value;
+	reserve.enabled = true;
+	postRes(reserve);
 }
 
 function postRes(reservation) {
+	console.log(reservation);
 	let resJson = JSON.stringify(reservation);
 	let xhr = new XMLHttpRequest();
 	let uri = 'api/reservation';
@@ -96,4 +125,17 @@ function postRes(reservation) {
 		}
 	}
 	xhr.send(resJson);
+}
+
+function updateRes(){
+	let form = document.resFormUpdate;
+	let reserve = {};
+	reserve.name = form.name.value;
+	reserve.reservationTime = form.reservationTime.value;
+	reserve.phone = form.phone.value;
+	reserve.howMany = form.howMany.value;
+	reserve.requests = form.requests.value;
+	reserve.email = form.email.value;
+	reserve.enabled = true;
+	postRes(reserve);
 }
